631c15353eac87b1b7c66a6053af58dcfb1ff661
cielomh22-xd <cielomh22@gmail.com>
2025-08-21 15:34:17 -0400

    modificamos welcome y web y creamos controller

--- TiendaGit/routes/web.php
+++ TiendaGit/routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\PersonaController;
+use App\Http\Controllers\ProductoController;
+use App\Http\Controllers\VentaController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/persona', [PersonaController::class, 'index']) ->name('persona.index');
+Route::get('/producto', [ProductoController::class, 'index']) ->name('producto.index');
+Route::get('/venta', [VentaController::class, 'index']) ->name('venta.index');
